3eb21c8d828a9927606fab3354db0a3ad471cddf
Aamir <aamir.nu.206@gmail.com>
2018-12-20 15:38:09 +0500

    URL correction

--- routers/urls.py
+++ routers/urls.py
@@ -4,7 +4,7 @@ from routers.views import RouterListView, RouterDetailView, RouterCardsListView,
 
 urlpatterns = [
     path('', RouterListView.as_view(), name='routers_list'),
-    path('<int:pk>/$', RouterDetailView.as_view(), name='routers_detail'),
+    path('<int:pk>/', RouterDetailView.as_view(), name='routers_detail'),
     path('<int:pk>/neighbours/', RouterNeighboursListView.as_view(), name='routers_detail'),
     path('<int:router_id>/cards/', RouterCardsListView.as_view(), name='cards_list'),
 ]
